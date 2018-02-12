474416c9cadaafd3f6723dfda353d0fbd6cd1ab2
jiasen.li@gmail.com <jiasen.li@gmail.com>
2018-02-12 21:33:13 +0800

    测试

--- template/src/views/layout/components/Sidebar/SidebarItem.vue
+++ template/src/views/layout/components/Sidebar/SidebarItem.vue
@@ -12,7 +12,7 @@
       <el-submenu v-else :index="item.name||item.path" :key="item.name">
         <template slot="title">
           <svg-icon v-if="item.meta&&item.meta.icon" :icon-class="item.meta.icon"></svg-icon>
-          <span v-if="item.meta&&item.meta.title">{{item.meta.title}}</span>
+          <span v-if="item.meta&&item.meta.title">\{{item.meta.title}}</span>
         </template>
 
         <template v-for="child in item.children" v-if="!child.hidden">
@@ -21,7 +21,7 @@
           <router-link v-else :to="item.path+'/'+child.path" :key="child.name">
             <el-menu-item :index="item.path+'/'+child.path">
               <svg-icon v-if="child.meta&&child.meta.icon" :icon-class="child.meta.icon"></svg-icon>
-              <span v-if="child.meta&&child.meta.title">{{child.meta.title}}</span>
+              <span v-if="child.meta&&child.meta.title">\{{child.meta.title}}</span>
             </el-menu-item>
           </router-link>
         </template>
